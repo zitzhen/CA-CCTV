580d66bb0296d5fbba2a99788274dbc6730ae071
oliver <oliverxiaozhen@outlook.com>
2026-05-28 23:40:03 +0800

    feat: skip empty lines when reading domains.txt

--- ct_watch.py
+++ ct_watch.py
@@ -1,6 +1,9 @@
 domains = open("domains.txt", mode='r',encoding="utf-8")
-# Filter comment lines
+# Filter comment lines and empty lines
 for line in domains:
+    line = line.strip()
+    if not line:
+       continue
     if line.startswith("#"):
         continue
     print(line)
